0b20f23b0752cb697474d553752c99425a42bff8
Govindarajan V <govindarajanv@outlook.com>
2021-04-15 07:31:23 +0000

    changes

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,7 @@ LABEL version="0.1.0"
 LABEL repository="https://github.com/govindarajanv/docker-actions"
 LABEL maintainer="Govindarajan V"
 
+#Docker caches the command so update and install should be in the same command
+RUN apt-get update && apt-get install jq -y 
 COPY entrypoint.sh /
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-set -U
+set -u
+
+if [ "$GITHUB_EVENT_NAME" != "milestone" ]; then
+    echo "::debug::The event name was'$GITHUB_EVENT_NAME'"
+    exit 0
+fi 
+
+event_type=$(jq --raw-output .action $GITHUB_EVENT_PATH)
+
+if [ $event_type != "closed" ]; then
+    echo "::debug::The event type was $event_type"
+    exit 0
+fi 
+
+milestone_name=$(jq --raw-output .milestone.title $GITHUB_EVENT_PATH)
+
 
 #logging commands
 echo "::set-output name-release-url::http://example.com"
